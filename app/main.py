f012c5dc358cd5dd2c6aed21cb6e89783b3db515
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-10-22 16:06:57 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class OnlineCourse:
             return (days + 6) // 7
 
         @classmethod
-        def from_dict(cls, course_dict: Dict[str, str]) - "OnlineCourse":
+        def from_dict(cls, course_dict: Dict[str, str]) -> "OnlineCourse":
             name = course_dict["name"]
             description = course_dict["description"]
             days = course_dict["days"]
