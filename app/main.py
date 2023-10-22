a37e37fb737140a46c6527288a5405cada5b87d1
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-10-22 16:04:50 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
+from typing import Dict
+
+
 class OnlineCourse:
-    def __init__(self, name, description, weeks):
+    def __init__(self, name: str, description: str, weeks: str):
         self.name = name
         self.description = description
         self.weeks = weeks
 
         @staticmethod
-        def days_to_weeks(days):
+        def days_to_weeks(days: int) -> int:
             return (days + 6) // 7
 
         @classmethod
-        def from_dict(cls, course_dict):
+        def from_dict(cls, course_dict: Dict[str, str]) - "OnlineCourse":
             name = course_dict["name"]
             description = course_dict["description"]
             days = course_dict["days"]
